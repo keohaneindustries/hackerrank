98ed8f2877263733aa3737e030abd1343c6e8d88
Luke Keohane <lukeohane@gmail.com>
2018-07-01 17:04:23 -0500

    completed inheritance challenge

--- hackerrank.py
+++ hackerrank.py
@@ -652,9 +652,60 @@ def hourglass():
     
     return
 
+def inheritance():
+    ### START of fixed code
+    class Person:
+        
+        def __init__(self, firstName, lastName, idNumber):
+            self.firstName = firstName
+            self.lastName = lastName
+            self.idNumber = idNumber
+        
+        def printPerson(self):
+            print("Name:", self.lastName + ",", self.firstName)
+            print("ID:", self.idNumber)
+
+    class Student(Person):
+    ### END of fixed code
+    
+        def __init__(self,firstName, lastName, idNumber,scores):
+            super().__init__(firstName, lastName, idNumber)
+            self.scores = scores
+
+        def calculate(self):
+            avg = sum(self.scores) / len(self.scores)
+            if (avg >= 90) and (avg <= 100):
+                grade = 'O'
+            elif (avg >= 80) and (avg <= 90):
+                grade = 'E'
+            elif (avg >= 70) and (avg <= 80):
+                grade = 'A'
+            elif (avg >= 55) and (avg <= 70):
+                grade = 'P'
+            elif (avg >= 40) and (avg <= 55):
+                grade = 'D'
+            else:
+                grade = 'T'
+            return grade
+    
+    
+    ### START of fixed code
+    line = input().split()
+    firstName = line[0]
+    lastName = line[1]
+    idNum = line[2]
+    numScores = int(input())  # not needed for Python
+    scores = list(map(int, input().split()))
+    s = Student(firstName, lastName, idNum, scores)
+    s.printPerson()
+    print("Grade:", s.calculate())
+    
+    return
+
+
 
 def main():
-    hourglass()
+    inheritance()
     return
 
 
