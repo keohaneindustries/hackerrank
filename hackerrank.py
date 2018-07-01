f09bdced8196f8c05a5b326facdf27dbc62bf2bb
Luke Keohane <lukeohane@gmail.com>
2018-07-01 18:17:03 -0500

    completed queues and stacks challenge

--- hackerrank.py
+++ hackerrank.py
@@ -850,8 +850,63 @@ def exceptions_2():
     return
     
 
+def queues():
+    ### START of fixed code
+    import sys
+
+    class Solution:
+    ### END of fixed code
+        
+        def __init__(self):
+            self.stack = []
+            self.queue = []
+        
+        def enqueueCharacter(self,char):
+            self.queue.insert(0,char)
+        
+        def pushCharacter(self,char):
+            self.stack.append(char)
+        
+        def popCharacter(self):
+            return self.stack.pop()
+        
+        def dequeueCharacter(self):
+            return self.queue.pop()
+    
+    
+    ### START of fixed code
+    # read the string s
+    s = input()
+    # Create the Solution class object
+    obj = Solution()
+
+    l = len(s)
+    # push/enqueue all the characters of string s to stack
+    for i in range(l):
+        obj.pushCharacter(s[i])
+        obj.enqueueCharacter(s[i])
+
+    isPalindrome = True
+    '''
+    pop the top character from stack
+    dequeue the first character from queue
+    compare both the characters
+    '''
+    for i in range(l // 2):
+        if obj.popCharacter() != obj.dequeueCharacter():
+            isPalindrome = False
+            break
+    # finally print whether string s is palindrome or not.
+    if isPalindrome:
+        print("The word, " + s + ", is a palindrome.")
+    else:
+        print("The word, " + s + ", is not a palindrome.")
+    
+    return
+
+
 def main():
-    exceptions_2()
+    queues()
     return
 
 
