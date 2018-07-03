d09114265f6f42060823bbe3a74350a8bea8302b
Luke Keohane <lukeohane@gmail.com>
2018-07-03 07:37:07 -0500

    completed linked list challenge #2

--- hackerrank.py
+++ hackerrank.py
@@ -1092,8 +1092,63 @@ def primality():
         print(my_calculator.is_prime(n))
 
 
+def linked_lists_2():
+    ### START of fixed code
+    class Node:
+        
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+    
+    class Solution:
+        
+        def insert(self, head, data):
+            p = Node(data)
+            if head == None:
+                head = p
+            elif head.next == None:
+                head.next = p
+            else:
+                start = head
+                while (start.next != None):
+                    start = start.next
+                start.next = p
+            return head
+        
+        def display(self, head):
+            current = head
+            while current:
+                print(current.data, end=' ')
+                current = current.next
+
+    ### END of fixed code
+        def removeDuplicates(self,head):
+            if not head:
+                return head
+            elif not head.next:
+                return head
+            else:
+                if head.next.data == head.data:
+                    head.next = head.next.next
+                    self.removeDuplicates(head)
+                else:
+                    self.removeDuplicates(head.next)
+                return head
+    
+    ### START of fixed code
+    mylist = Solution()
+    T = int(input())
+    head = None
+    for i in range(T):
+        data = int(input())
+        head = mylist.insert(head, data)
+    head = mylist.removeDuplicates(head)
+    mylist.display(head)
+    return
+
+
 def main():
-    primality()
+    linked_lists_2()
     return
 
 
