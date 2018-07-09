85a4284f81293025fabdf6b9045ee6db4ba75cc4
Luke Keohane <lukeohane@gmail.com>
2018-07-09 07:06:04 -0500

    completed testing challenge

--- hackerrank.py
+++ hackerrank.py
@@ -1170,8 +1170,89 @@ def nested_logic():
     return
 
 
+def testing():
+    ### START of fixed code
+    import array
+    def minimum_index(seq):
+        if len(seq) == 0:
+            raise ValueError("Cannot get the minimum value index from an empty sequence")
+        min_idx = 0
+        for i in range(1, len(seq)):
+            if seq[i] < seq[min_idx]:
+                min_idx = i
+        return min_idx
+    
+    ### END of fixed code
+
+    class TestDataEmptyArray(object):
+        
+        @staticmethod
+        def get_array():
+            import array
+            return array.array('i')
+
+    class TestDataUniqueValues(object):
+    
+        @staticmethod
+        def get_array():
+            import array
+            l= [-1,5,8,7,0]
+            return array.array('i',l)
+    
+        @staticmethod
+        def get_expected_result():
+            return 0
+
+    class TestDataExactlyTwoDifferentMinimums(object):
+    
+        @staticmethod
+        def get_array():
+            import array
+            l = [-1, 5, 8, 7, 0,-1]
+            return array.array('i', l)
+    
+        @staticmethod
+        def get_expected_result():
+            return 0
+    
+    ### START of fixed code
+    def TestWithEmptyArray():
+        try:
+            seq = TestDataEmptyArray.get_array()
+            result = minimum_index(seq)
+        except ValueError as e:
+            pass
+        else:
+            assert False
+
+    def TestWithUniqueValues():
+        seq = TestDataUniqueValues.get_array()
+        assert len(seq) >= 2
+    
+        assert len(list(set(seq))) == len(seq)
+    
+        expected_result = TestDataUniqueValues.get_expected_result()
+        result = minimum_index(seq)
+        assert result == expected_result
+
+    def TestiWithExactyTwoDifferentMinimums():
+        seq = TestDataExactlyTwoDifferentMinimums.get_array()
+        assert len(seq) >= 2
+        tmp = sorted(seq)
+        assert tmp[0] == tmp[1] and (len(tmp) == 2 or tmp[1] < tmp[2])
+    
+        expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result()
+        result = minimum_index(seq)
+        assert result == expected_result
+
+    TestWithEmptyArray()
+    TestWithUniqueValues()
+    TestiWithExactyTwoDifferentMinimums()
+    print("OK")
+
+
 def main():
-    nested_logic()
+    testing()
     return
 
 
