a6dccee5ffd03fc5180dc2ca0256ba20c0580580
Luke Keohane <lukeohane@gmail.com>
2018-07-09 07:26:49 -0500

    completed regex challenge

--- hackerrank.py
+++ hackerrank.py
@@ -1251,8 +1251,33 @@ def testing():
     print("OK")
 
 
+def regex():
+    import math
+    import os
+    import random
+    import re
+    import sys
+    
+    if __name__ == '__main__':
+        N = int(input())
+        names = []
+        for N_itr in range(N):
+            firstNameEmailID = input().split()
+            
+            firstName = firstNameEmailID[0]
+            
+            emailID = firstNameEmailID[1]
+            
+            if re.search('(?<=@)gmail\.com$',emailID):
+                names.append(firstName)
+        
+        names.sort()
+        for name in names:
+            print(name)
+
+
 def main():
-    testing()
+    regex()
     return
 
 
