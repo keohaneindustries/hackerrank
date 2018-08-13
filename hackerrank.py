086fb1686adb992023145e6f64b3621541642e90
Luke Keohane <lukeohane@gmail.com>
2018-08-13 06:48:10 -0500

    completed permuting two arrays challenge

--- hackerrank.py
+++ hackerrank.py
@@ -1596,8 +1596,37 @@ def cutting_boards():
     return
 
 
+def permuting_two_arrays():
+    def twoArrays(k, A, B):
+        A.sort()
+        B.sort(reverse=True)
+        if (any(a + b < k for a, b in zip(A, B))):
+            return "NO"
+        else:
+            return "YES"
+
+    q = int(input())
+
+    for q_itr in range(q):
+        nk = input().split()
+    
+        n = int(nk[0])
+    
+        k = int(nk[1])
+    
+        A = list(map(int, input().rstrip().split()))
+    
+        B = list(map(int, input().rstrip().split()))
+    
+        result = twoArrays(k, A, B)
+        
+        print(result)
+    
+    return
+
+
 def main():
-    cutting_boards()
+    permuting_two_arrays()
     return
 
 
