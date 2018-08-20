fc0be1db218879ee22092883d4650ba2ba850ac1
Luke Keohane <lukeohane@gmail.com>
2018-08-20 06:16:51 -0500

    more work on Cloudy Day challenge

--- hackerrank.py
+++ hackerrank.py
@@ -1667,63 +1667,41 @@ def cloudy_day():
         def _sum_sunny(p,x,clouds):
             # for each cloud find cities made sunny by removing
             if len(clouds) == 0:
-                sunny=sum(p)
+                return sum(p)
             else:
-                s = []
-                is_dark = False
-                for j, town in enumerate(x):
-                    for i in clouds:
-                        if i[0] <= town <= i[1]:
-                            is_dark = True
-                            break
-                    if not is_dark:
-                        s.append(j)
-                    is_dark = False
-                sunny=sum([p[i] for i in s])
-            return sunny
+                return sum(p)-sum([p[i] for i in set([j for j, town in enumerate(x) for i in clouds if i[0] <= town <= i[1]])])
         
-        sunny_ppl = 0
-        ord = (lambda s: sorted(range(1, len(s) + 1), key=lambda i: s[i - 1]))(x)
-        x_sort = [x[i - 1] for i in ord]
-        p_sort = [p[i - 1] for i in ord]
+        
+        x_simple = list(set(x))
+        p_simple = [sum([p[j] for j, town_coord in enumerate(x) if x_coord==town_coord]) for x_coord in x_simple]
+        
+        # ord = (lambda s: sorted(range(1, len(s) + 1), key=lambda i: s[i - 1]))(x)
+        # x_sort = [x[i - 1] for i in ord]
+        # p_sort = [p[i - 1] for i in ord]
         clouds = [[y[i] - r[i], y[i] + r[i]] for i in range(len(r))]
-        ord_clouds = (lambda s: sorted(range(1, len(s) + 1), key=lambda i: s[i - 1][0]))(clouds)
-        clouds_sort = [clouds[i - 1] for i in ord_clouds]
+        # ord_clouds = (lambda s: sorted(range(1, len(s) + 1), key=lambda i: s[i - 1][0]))(clouds)
+        # clouds_sort = [clouds[i - 1] for i in (lambda s: sorted(range(1, len(s) + 1), key=lambda i: s[i - 1][0]))(clouds)]
 
         # find list of clouds that cover at least one town
-        l = []
-        for i, cd in enumerate(clouds_sort):
-            for j in x_sort:
-                if cd[0] <= j <= cd[1]:
-                    l.append(i)
-                    break
-        clouds_filter = [clouds_sort[i] for i in l]
+        clouds_filter = [clouds[i] for i, cd in enumerate(clouds) for j in x_simple if cd[0] <= j <= cd[1]]
+        # clouds_filter = [clouds_sort[i] for i, cd in enumerate(clouds_sort) for j in x_sort if cd[0] <= j <= cd[1]]
         
         if len(clouds_filter)==0:
-            sunny_ppl=sum(p_sort)
+            return sum(p_simple)
+            # return sum(p_sort)
         else:
-            # find list of towns that are already sunny
-            s = []
-            d =[]
-            is_dark=False
-            for j,town in enumerate(x_sort):
-                for i in clouds_filter:
-                    if i[0] <= town <= i[1]:
-                        d.append(j)
-                        is_dark=True
-                        break
-                if not is_dark:
-                    s.append(j)
-                is_dark=False
-                
-            x_sunny = [x_sort[i] for i in s]
-            p_sunny = sum([p_sort[i] for i in s])
-            x_dark = [x_sort[i] for i in d]
-            p_dark = [p_sort[i] for i in d]
+            # find list of towns that are not yet sunny
+            d = set([j for j, town in enumerate(x_simple) for i in clouds_filter if i[0] <= town <= i[1]])
+            # d = set([j for j, town in enumerate(x_sort) for i in clouds_filter if i[0] <= town <= i[1]])
+            
+            x_dark = [x_simple[i] for i in d]
+            p_dark = [p_simple[i] for i in d]
+            p_sunny = sum(p_simple) - sum(p_dark)
+            # x_dark = [x_sort[i] for i in d]
+            # p_dark = [p_sort[i] for i in d]
+            # p_sunny = sum(p_sort) - sum(p_dark)
 
-            sunny_add= max([_sum_sunny(p_dark,x_dark,clouds_filter[:i]+clouds_filter[i+1:]) for i in range(len(clouds_filter))])
-            sunny_ppl=sunny_add+p_sunny
-        return sunny_ppl
+            return p_sunny+max([_sum_sunny(p_dark,x_dark,clouds_filter[:i]+clouds_filter[i+1:]) for i in range(len(clouds_filter))])
 
     n = int(input())
 
