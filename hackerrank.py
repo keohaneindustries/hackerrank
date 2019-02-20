ddca33ce96c57632625ae702955d45fbf4085e16
Luke Keohane <lukeohane@gmail.com>
2019-02-20 05:59:26 -0600

    completed ZigZagSequence

--- hackerrank.py
+++ hackerrank.py
@@ -2085,8 +2085,37 @@ def larrys_array():
     return
 
 
+def zig_zag_sequence():
+    def findZigZagSequence(a, n):
+        a.sort()
+        mid = int((n - 1) / 2)
+        a[mid], a[mid+1:] = a[-1], a[mid:-1]
+        
+        st = mid + 1
+        ed = n - 1
+        while (st <= ed):
+            a[st], a[ed] = a[ed], a[st]
+            st = st + 1
+            ed = ed - 1
+        
+        for i in range(n):
+            if i == n - 1:
+                print(a[i])
+            else:
+                print(a[i], end=' ')
+        return
+
+    test_cases = int(input())
+    for cs in range(test_cases):
+        n = int(input())
+        a = list(map(int, input().split()))
+        findZigZagSequence(a, n)
+    
+    return
+
+
 def main():
-    larrys_array()
+    zig_zag_sequence()
     return
 
 
